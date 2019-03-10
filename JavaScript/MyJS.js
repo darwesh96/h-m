f5fe50c76e9bb3e7bb3f0fac64b55a6265ef1c4a
mohamed darwesh <mohamed_darwesh123@hotmail.com>
2019-03-10 20:49:09 +0200

    Adding all files

---
+++ JavaScript/MyJS.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function () {
+	var scrollTop = 0;
+	var Logo = document.getElementById("Logo")
+
+	$(window).scroll(function () {
+		scrollTop = $(window).scrollTop();
+		$('.counter').html(scrollTop);
+
+		if (scrollTop >= 100) {
+			$('#global-nav').addClass('scrolled-nav');
+			Logo.style.width = '40px';
+
+
+		} else if (scrollTop < 100) {
+			$('#global-nav').removeClass('scrolled-nav');
+			Logo.style.width = '60px';
+		}
+
+	});
+
+});
+
+$(document).ready(function () {
+	$('#camp-toggler').click(function () {
+		$('#camp').toggleClass('CAMPAIGN');
+		$('#ul').toggleClass('ul-togger');
+	});
+});
+
+
+
+
+
